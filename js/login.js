9f63bb4f80249d36a702781eb0ceda0a49f16508
ShohruxSafarmatov <safarmatov.shohrux@gmail.com>
2023-06-19 07:50:37 +0500

    login page add

---
+++ js/login.js
@@ -0,0 +1,8 @@
+let elForm = document.querySelector(".login__form");
+let elNameInp = document.querySelector(".login__name");
+let elPassInp = document.querySelector(".login__pass");
+
+
+elForm.addEventListener('submit', (event) => {
+
+})
